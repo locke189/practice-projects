62efa928bd0704944246ff69fe4bd021331f9c28
Juan Insuasti <juan.insuasti@gmail.com>
2023-02-14 09:19:50 -0500

    progress steps fix

--- progress-steps/index.js
+++ progress-steps/index.js
@@ -1,22 +1,24 @@
 const nextBtnDOM = document.querySelector(".steps__next");
 const prevBtnDOM = document.querySelector(".steps__prev");
-const stepsDOM = Array.from(document.querySelectorAll(".steps__step--js"));
+const stepsDOM = Array.from(document.querySelectorAll(".steps__step"));
 const reverseStepsDOM = [...stepsDOM].reverse();
 
+let activeIndex = 0;
+
 nextBtnDOM.addEventListener("click", () => {
-  const nextStep = stepsDOM.find((step) => {
-    return !step.classList.contains("steps__step--active");
-  });
-  if (nextStep) {
-    nextStep.classList.add("steps__step--active");
+  if (activeIndex < stepsDOM.length - 1) {
+    activeIndex++;
+    stepsDOM[activeIndex].classList.add("steps__step--active");
+    prevBtnDOM.disabled = false;
+    nextBtnDOM.disabled = activeIndex === stepsDOM.length - 1 ? true : false;
   }
 });
 
 prevBtnDOM.addEventListener("click", () => {
-  const nextStep = reverseStepsDOM.find((step) => {
-    return step.classList.contains("steps__step--active");
-  });
-  if (nextStep) {
-    nextStep.classList.remove("steps__step--active");
+  if (activeIndex > 0) {
+    stepsDOM[activeIndex].classList.remove("steps__step--active");
+    activeIndex--;
+    nextBtnDOM.disabled = false;
+    prevBtnDOM.disabled = activeIndex === 0 ? true : false;
   }
 });
